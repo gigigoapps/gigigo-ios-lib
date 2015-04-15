5802ca0a51eeae2bef9a88766d14a322af627f47
sergio.baro <sergio.baro@gigigo.com>
2015-04-15 13:34:45 +0200

    Added GIGGeolocation

---
+++ GIGGeolocation/GIGGeolocation.h
@@ -0,0 +1,21 @@
+//
+//  GIGGeolocation.h
+//  ClubVips
+//
+//  Created by Sergio Baró on 18/03/14.
+//  Copyright (c) 2014 gigigo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <CoreLocation/CoreLocation.h>
+
+
+typedef void(^GIGGeolocationCompletion)(BOOL success, BOOL authorized, CLLocation *location, NSError *error);
+
+
+@interface GIGGeolocation : NSObject
+
+- (void)locateCompletion:(GIGGeolocationCompletion)completion;
+
+@end
---
+++ GIGGeolocation/GIGGeolocation.m
@@ -0,0 +1,122 @@
+//
+//  GIGGeolocation.m
+//  ClubVips
+//
+//  Created by Sergio Baró on 18/03/14.
+//  Copyright (c) 2014 gigigo. All rights reserved.
+//
+
+#import "GIGGeolocation.h"
+
+
+@interface GIGGeolocation ()
+<CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (copy, nonatomic) GIGGeolocationCompletion completion;
+
+@end
+
+
+@implementation GIGGeolocation
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self != nil)
+	{
+		self.locationManager = [[CLLocationManager alloc] init];
+		self.locationManager.delegate = self;
+	}
+	return self;
+}
+
+#pragma mark - Public
+
+- (void)locateCompletion:(GIGGeolocationCompletion)completion
+{
+    self.completion = completion;
+    [self start];
+}
+
+#pragma mark - Private
+
+- (void)updateLocation:(CLLocation *)location
+{
+    if (self.completion)
+    {
+        self.completion(YES, YES, location, nil);
+    }
+    
+    [self stop];
+}
+
+- (void)start
+{
+    if ([CLLocationManager locationServicesEnabled])
+    {
+        if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)])
+        {
+            [self.locationManager requestWhenInUseAuthorization];
+        }
+        
+        [self.locationManager startUpdatingLocation];
+    }
+}
+
+- (void)stop
+{
+    self.completion = nil;
+    [self.locationManager stopUpdatingLocation];
+}
+
+#pragma mark - Private
+
+- (BOOL)isAuthorizedStatus:(CLAuthorizationStatus)status
+{
+    return (status == kCLAuthorizationStatusAuthorizedWhenInUse || status == kCLAuthorizationStatusAuthorized);
+}
+
+#pragma mark - CLLocationManager Delegate
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if ([self isAuthorizedStatus:status])
+    {
+        [self.locationManager startUpdatingLocation];
+    }
+    else if (self.completion)
+    {
+        self.completion(NO, NO, nil, nil);
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+	[self updateLocation:newLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+	[self updateLocation:[locations lastObject]];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    if (self.completion)
+    {
+        BOOL authorized = [self isAuthorizedStatus:CLLocationManager.authorizationStatus];
+        if (self.locationManager.location)
+        {
+            self.completion(YES, authorized, self.locationManager.location, error);
+        }
+        else
+        {
+            self.completion(NO, authorized, nil, error);
+        }
+    }
+    
+    [self stop];
+}
+
+@end
