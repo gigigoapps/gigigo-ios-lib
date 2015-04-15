f5ad305e8277e60fc0274a2b8adffad97b1aaae6
sergio.baro <sergio.baro@gigigo.com>
2015-04-15 15:31:36 +0200

    Added UIColor extension

---
+++ GIGUtils/GIGUtils.h
@@ -0,0 +1,24 @@
+//
+//  GIGUtils.h
+//  copiloto
+//
+//  Created by Sergio Baró on 15/04/15.
+//  Copyright (c) 2015 Gigigo. All rights reserved.
+//
+
+#ifndef copiloto_GIGUtils_h
+#define copiloto_GIGUtils_h
+
+// functions
+#import "GIGDispatch.h"
+
+// classes
+#import "GIGDigest.h"
+#import "GIGFileManager.h"
+
+// categories
+#import "UIColor+GIGExtension.h"
+#import "NSBundle+GIGLibrary.h"
+#import "NSUserDefaults+GIGArchive.h"
+
+#endif
