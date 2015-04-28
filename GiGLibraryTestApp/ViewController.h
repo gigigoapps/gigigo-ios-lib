61b136f8e90a0e50cffb4cd5cf948ae92f0df5e7
Alejandro Jimenez <alejandro.jimenez@gigigo.com>
2015-04-28 19:14:31 +0200

    - Creado Framework y reestructuración del código - El Framework provee (en teoría) el GiGURLRequest

---
+++ GiGLibraryTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiGLibraryTestApp
+//
+//  Created by Alejandro Jiménez Agudo on 28/4/15.
+//  Copyright (c) 2015 Gigigo SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
