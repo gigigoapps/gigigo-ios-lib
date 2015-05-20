7f28ebb6f15bbbb23a732004714a72242de2d5b5
sergio.baro <sergio.baro@gigigo.com>
2015-05-20 11:53:14 +0200

    Added App from source code

---
+++ GIGLibrarySourceApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIGLibrarySourceApp
+//
+//  Created by Sergio Baró on 20/05/15.
+//  Copyright (c) 2015 Gigigo SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIGLibrarySourceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIGLibrarySourceApp
+//
+//  Created by Sergio Baró on 20/05/15.
+//  Copyright (c) 2015 Gigigo SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
