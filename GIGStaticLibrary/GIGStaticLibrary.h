fb40ef1a875d00879cb5421e2896406e4218216d
Judith Medina <judith.medina@gigigo.com>
2015-05-20 17:03:34 +0200

    Created the static library target and the framework target.

---
+++ GIGStaticLibrary/GIGStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  GIGStaticLibrary.h
+//  GIGStaticLibrary
+//
+//  Created by Judith Medina on 20/5/15.
+//  Copyright (c) 2015 Gigigo SL. All rights reserved.
+//
+
+#import<UIKit/UIKit.h>
+
+#import <GIGStaticLibrary/GIGStaticLibrary.h>
+#import <GIGStaticLibrary/GIGNetwork.h>
+#import <GIGStaticLibrary/GIGConstants.h>
