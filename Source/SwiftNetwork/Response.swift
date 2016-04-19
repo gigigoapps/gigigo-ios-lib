5a39c167640ac5f69b08aeb848b84bf40d9b8dee
Alejandro Jiménez <a.j.agudo@gmail.com>
2016-04-19 21:46:36 +0200

    - SwiftNetwork: Added methods to the Response class that returns a JSON or a UIImage

--- Source/SwiftNetwork/Response.swift
+++ Source/SwiftNetwork/Response.swift
@@ -151,3 +151,28 @@ public class Response {
 	}
 	
 }
+
+
+enum ResponseError: ErrorType {
+	case BodyNil
+}
+
+extension Response {
+	
+	func json() throws -> JSON {
+		guard let json = self.body else {
+			throw ResponseError.BodyNil
+		}
+		
+		return JSON(json: json)
+	}
+	
+	func image() throws -> UIImage {
+		guard let image = self.body as? UIImage else {
+			throw ResponseError.BodyNil
+		}
+		
+		return image
+	}
+	
+}
