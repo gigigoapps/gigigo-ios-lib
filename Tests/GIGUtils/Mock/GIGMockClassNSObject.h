8b3c68902e794fcd8563434b397ca400c3903c81
Carlos Vicente <carlos.vicente@gigigo.com>
2016-06-20 12:32:16 +0200

    Added methods to archive/unarchive an array in NSUserDefaults+GigArchive category

--- Tests/GIGUtils/Mock/GIGMockClassNSObject.h
+++ Tests/GIGUtils/Mock/GIGMockClassNSObject.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface GIGMockClassNSObject : NSObject
+@interface GIGMockClassNSObject : NSObject <NSCoding>
 
 @property (strong, nonatomic) NSString *stringNSObject;
 @property (assign, nonatomic) NSInteger integerNSObject;
