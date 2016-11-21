a0e5fc2117e97d4a20371fdc1829c14a2b641cb8
Alejandro Jiménez Agudo <alejandro.jimenez@gigigo.com>
2016-11-21 18:41:56 +0100

    - Added appVersion to Application wrapper

--- Source/GIGUtils/Application/Application.swift
+++ Source/GIGUtils/Application/Application.swift
@@ -13,6 +13,21 @@ open class Application {
 	
 	public init(){}
 	
+	
+	// MARK: - App Info
+	
+	/// Returns the application short version
+	public var appVersion: String {
+		guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
+			return "0.0"
+		}
+		
+		return version
+	}
+	
+	
+	// MARK: - App Actions
+	
 	open func presentModal(_ viewController: UIViewController) {
 		let topVC = self.topViewController()
         DispatchQueue.main.async(execute: {
