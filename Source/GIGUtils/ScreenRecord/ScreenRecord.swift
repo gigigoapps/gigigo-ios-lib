00ce26165a27df6175a74cb922b4562ac221072f
eduardo parada pardo <udelunar@gmail.com>
2018-04-25 15:49:43 +0200

    Update: Update styles, whiteSpace, and control error

--- Source/GIGUtils/ScreenRecord/ScreenRecord.swift
+++ Source/GIGUtils/ScreenRecord/ScreenRecord.swift
@@ -1,6 +1,6 @@
 //
 //  ScreenRecord.swift
-//  BORRAR1
+//  GIGLibrary
 //
 //  Created by eduardo parada pardo on 25/4/18.
 //  Copyright © 2018 eduardo parada pardo. All rights reserved.
@@ -36,7 +36,7 @@ extension ScreenRecord: ScreenRecordInput {
     
     func startRecording() {
         if #available(iOS 10.0, *) {
-            self.recorder.startRecording{ [unowned self] (error) in
+            self.recorder.startRecording { [unowned self] (error) in
                 if let unwrappedError = error {
                     self.view?.errorRecording(error: unwrappedError)
                 }
@@ -51,6 +51,9 @@ extension ScreenRecord: ScreenRecordInput {
                     unwrappedPreview.previewControllerDelegate = self
                     self.view?.showPreview(viewController: unwrappedPreview)
                 }
+                if let unwrappedError = error {
+                    self.view?.errorRecording(error: unwrappedError)
+                }
             }
         }
     }
@@ -63,5 +66,3 @@ extension ScreenRecord: RPPreviewViewControllerDelegate {
         self.view?.dismissView()
     }
 }
-
-
