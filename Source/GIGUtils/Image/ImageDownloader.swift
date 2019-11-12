c418c7a757d568de8bcb55c8851ea8293ec557a7
Eduardo González <eduardo.gonzalez@gigigo.com>
2019-11-12 10:12:32 +0100

    Update: ImageDownloader refactor.

--- Source/GIGUtils/Image/ImageDownloader.swift
+++ Source/GIGUtils/Image/ImageDownloader.swift
@@ -11,94 +11,76 @@ import UIKit
 struct ImageDownloader {
 	
 	static let shared = ImageDownloader()
-	static var queue: [UIImageView: Request] = [:]
-	static var stack: [UIImageView] = []
+	static var requestsQueue: [UIImageView: Request] = [:]
+	static var views: [UIImageView] = []
 	static var images: [String: UIImage] = [:]
 	
 	
 	private init() {
         NotificationCenter.default.addObserver(forName: UIApplication.didReceiveMemoryWarningNotification, object: nil, queue: nil) { _ in
 			ImageDownloader.images = [:]
-			ImageDownloader.stack = []
-			ImageDownloader.queue = [:]
+			ImageDownloader.views = []
+			ImageDownloader.requestsQueue = [:]
 		}
 	}
 	
 	// MARK: - Public methods
 	
 	func download(url: String, for view: UIImageView, placeholder: UIImage?) {
-		if let request = ImageDownloader.queue[view] {
-			request.cancel()
-			ImageDownloader.queue.removeValue(forKey: view)
-		}
-		
-        if let image = ImageDownloader.images[url] {
-            DispatchQueue.main.async {
-                view.image = image
-            }
-        } else {
+        if let request = ImageDownloader.requestsQueue[view] {
+            request.cancel()
+            ImageDownloader.requestsQueue.removeValue(forKey: view)
+        }
+        guard let image = ImageDownloader.images[url] else {
             DispatchQueue.main.async {
                 view.image = placeholder
             }
-            self.loadImage(url: url, in: view)
+            return self.createRequest(url: url, in: view)
+        }
+        DispatchQueue.main.async {
+            view.image = image
         }
     }
     
 	// MARK: - Private Helpers
 	
-	private func loadImage(url: String, in view: UIImageView) {
+	private func createRequest(url: String, in view: UIImageView) {
 		let request = Request(method: HTTPMethod.get.rawValue, baseUrl: url, endpoint: "")
-		ImageDownloader.queue[view] = request
-		ImageDownloader.stack.append(view)
+		ImageDownloader.requestsQueue[view] = request
+		ImageDownloader.views.append(view)
 		
-		if ImageDownloader.stack.count <= 3 {
+		if ImageDownloader.views.count <= 3 {
 			self.downloadNext()
 		}
 	}
     
-	private func downloadNext() {
-		guard let view = ImageDownloader.stack.popLast() else { return }
-		guard let request = ImageDownloader.queue[view] else {
+    private func downloadNext() {
+        DispatchQueue.global().async {
+            self.next()
+        }
+    }
+    
+	private func next() {
+		guard let view = ImageDownloader.views.first,
+            let request = ImageDownloader.requestsQueue[view] else {
             return
         }
+        ImageDownloader.views.removeFirst()
 		
 		request.fetch { response in
 			switch response.status {
-				
-			case .success:
+                
+            case .success:
                 if let image = try? response.image() {
                     DispatchQueue(label: "com.gigigo.imagedownloader", qos: .background).async {
-                        var finalImage = image
-                        let width = view.width() * UIScreen.main.scale
-                        let height = view.height() * UIScreen.main.scale
-                        if let resized = image.imageProportionally(with: CGSize(width: width, height: height)) {
-                            finalImage = resized
-                        }
-                        ImageDownloader.images.updateValue(finalImage, forKey: request.baseURL)
-
-                    
+                        let resizedImage = self.resize(image: image, forView: view)
                         DispatchQueue.main.async {
-                            if let currentRequest = ImageDownloader.queue[view], request.baseURL == currentRequest.baseURL {
-                                self.setAnimated(image: finalImage, in: view)
-                            }                    
-                            if let index = ImageDownloader.queue.index(forKey: view) {
-                                ImageDownloader.queue.remove(at: index)
-                            }
-                            self.downloadNext()
+                            self.handleSuccess(image: resizedImage, view: view, request: request)
                         }
                     }
                 } else if let imageGif = try? response.gif() {
                     DispatchQueue.main.async {
-                        ImageDownloader.images.updateValue(imageGif, forKey: request.baseURL)
-
-                        if let currentRequest = ImageDownloader.queue[view], request.baseURL == currentRequest.baseURL {
-                            self.setAnimated(image: imageGif, in: view)
-                        }
-                        
-                        if let index = ImageDownloader.queue.index(forKey: view) {
-                            ImageDownloader.queue.remove(at: index)
-                        }
-                        self.downloadNext()
+                        self.handleSuccess(image: imageGif, view: view, request: request)
                     }
                 } else {
                     LogWarn("Al descargar la imagen,o se ha recibido un body vacio o no se se ha reconocido el tipo de imagen que es.")
@@ -112,10 +94,30 @@ struct ImageDownloader {
 		}
 	}
 	
+    
+    private func handleSuccess(image: UIImage, view: UIImageView, request: Request) {
+        ImageDownloader.images.updateValue(image, forKey: request.baseURL)
+
+        if let currentRequest = ImageDownloader.requestsQueue[view], request.baseURL == currentRequest.baseURL {
+            self.setAnimated(image: image, in: view)
+        }
+        ImageDownloader.requestsQueue.removeValue(forKey: view)
+        self.downloadNext()
+    }
+    
+    private func resize(image: UIImage, forView view: UIView) -> UIImage {
+        let width = view.width() * UIScreen.main.scale
+        let height = view.height() * UIScreen.main.scale
+        guard let imageResized = image.imageProportionally(with: CGSize(width: width, height: height)) else {
+            return image
+        }
+        return imageResized
+    }
+    
 	private func setAnimated(image: UIImage?, in view: UIImageView) {
 		UIView.transition(
 			with: view,
-			duration:0.4,
+			duration:0.3,
 			options: .transitionCrossDissolve,
 			animations: {
 				view.image = image
